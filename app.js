786e7408f6c6b379b646d48355b33fb00740684a
Wilson Seabra <wil@taprapeixe.com.br>
2023-01-07 21:40:49 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const server = http.createServer(app)
+const io = new Server(server);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+let users = [];
+
+io.on("connection", (socket) => {
+    console.log(`User connected with id ${socket.id}`);
+    users = users.concat(socket.id);
+
+    console.log(`Connected users: ${users}`);
+
+    socket.on("clientMessage", (msgContent) => {
+        socket.emit("recebeuMsg", "Recebeu msg");
+
+        socket.broadcast.emit("groupMessage", msgContent);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
+
+const form = document.getElementById("comment-form");
+function enviarMensagem(){
+    console.log("teste")
+}
