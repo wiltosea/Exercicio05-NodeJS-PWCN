9e08183e8b569029f65e7f6768a4d5144916326f
Wilson Seabra <wil@taprapeixe.com.br>
2023-01-08 13:53:04 -0300

    exercicio finalizando.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = new Server(server);
 
 // view engine setup
@@ -18,27 +18,20 @@ app.set("view engine", "hbs");
 let users = [];
 
 io.on("connection", (socket) => {
-    console.log(`User connected with id ${socket.id}`);
-    users = users.concat(socket.id);
+  console.log(`User connected with id ${socket.id}`);
+  users = users.concat(socket.id);
 
-    console.log(`Connected users: ${users}`);
+  console.log(`Connected users: ${users}`);
 
-    socket.on("clientMessage", (msgContent) => {
-        socket.emit("recebeuMsg", "Recebeu msg");
-
-        socket.broadcast.emit("groupMessage", msgContent);
-    });
+  socket.on("sendMessage", (data) => {
+    socket.broadcast.emit("receiver", data);
+  });
 });
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 server.listen(3000, () => {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
-
-const form = document.getElementById("comment-form");
-function enviarMensagem(){
-    console.log("teste")
-}
